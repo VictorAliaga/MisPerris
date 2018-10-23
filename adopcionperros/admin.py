e4c6a8cb70a3be18136e0b77d3d2d5c2eb89d54d
VictorAliaga <vict.aliaga@alumnos.duoc.cl>
2018-10-23 01:11:19 -0300

    Agrega Models.py 1.0 Terminado, funcionado con detalles

--- adopcionperros/admin.py
+++ adopcionperros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Adoptador)
+admin.site.register(Mascotas)
+admin.site.register(AdopcionMascota)
--- adopcionperros/models.py
+++ adopcionperros/models.py
@@ -4,13 +4,13 @@ from django.utils import timezone
 # Clase Adoptadores
 
 class Adoptador(models.Model):
-    NombreUsuario = models.Charfield(max_length=20, required= True, primary_key=True)
-    ApellidoPaterno = models.Charfield(max_length=15, required= True)
-    ApellidoMaterno = models.Charfield(max_length=15, required= True)
-    NombreCompleto = models.Charfield(max_length=30, required= True)
-    Correo = models.Emailfield(max_length=50, required= True)
-    Contraseña = models.Charfield(max_length=15, required= True)
-    ConfirmarContraseña = models.Charfield(max_length=15, required= True)
+    NombreUsuario = models.CharField(max_length=20, primary_key=True)
+    ApellidoPaterno = models.CharField(max_length=15)
+    ApellidoMaterno = models.CharField(max_length=15)
+    NombreCompleto = models.CharField(max_length=30)
+    Correo = models.EmailField(max_length=50)
+    Contraseña = models.CharField(max_length=15)
+    ConfirmarContraseña = models.CharField(max_length=15)
 
     # Metodo para Confirmar la Contraseña con la Confirmar Contraseña para crear el Usuario Adoptador
     def confirmar_usuario(self):
@@ -25,10 +25,25 @@ class Adoptador(models.Model):
 
 class Mascotas(models.Model):
     Imagen = models.ImageField(upload_to='templates/adopcionperros/perrisimagenes/')
-    NombreMascota = models.Charfield(max_length=20, required= True, primary_key=True)
-    RazaPredominante = models.Charfield(max_length=15, required= True)
-    Descripcion = models.TextField(required= True)
-    Estado = models.Charfield(max_length=15, required= True)
+    NombreMascota = models.CharField(max_length=20,primary_key=True)
+    RazaPredominante = models.CharField(max_length=15)
+    Descripcion = models.TextField()
+    Estado = models.CharField(max_length=15)
+
+    # Retornamos el nombre de la mascota
+    def __str__(self):
+        return self.NombreMascota
+
+# Clase Formulario Adopcion Relacion Adoptador con Mascotas
+
+class AdopcionMascota(models.Model):
+    Adoptador = models.ForeignKey(Adoptador, null=False, blank=False, on_delete=models.CASCADE)
+    Mascotas = models.ForeignKey(Mascotas, null=False, blank=False, on_delete=models.CASCADE)
+
+    # Retornamos el Nombre de Usuario del Adoptador con la Mascota Adoptada
+    def __str__(self):
+        cadena = "{0} Ha Adoptado a {1}"
+        return cadena.format(self.Adoptador.NombreUsuario, self.Mascotas.NombreMascota)
 
 
 
