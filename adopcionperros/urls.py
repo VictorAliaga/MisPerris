cddc73ca3c0f8710947c4f521c417501c9866c3f
VictorAliaga <vict.aliaga@alumnos.duoc.cl>
2018-10-29 18:01:10 -0300

    Agrega Recuperar Contraseña, Agrega Boton Administracion

--- adopcionperros/urls.py
+++ adopcionperros/urls.py
@@ -3,6 +3,8 @@ from . import views
 # importamos settings y static para las imagenes de los perritos
 from django.conf import settings
 from django.conf.urls.static import static
+# importamos vistas predeterminadas en django
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 # REEMPLAZE post_list por index ... !!!
 urlpatterns = [
@@ -12,5 +14,13 @@ urlpatterns = [
     path('logout/', views.logout_view, name='vista_logout'),
     path('mascotas/', views.mascotas_view, name='vista_mascotas'),
     path('perrito/<int:pk>/', views.perrito_detalle_view, name='vista_perrito_detalle'),
+     path('accounts/login/', views.login_view, name ='vista_login'),
+
+    #Urls Recuperacion de Contraseña
+    path('reset/password/', PasswordResetView.as_view(), name='reset_password'),
+    path('reset/password/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/password/confirm/P<uidb64>P<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/password/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
     # url para las imagenes que suben de los perritos
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- misperris/settings.py
+++ misperris/settings.py
@@ -121,5 +121,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Aqui subimos las imagenes 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Recuperar contraseña
+
+EMAIL_USE_TLS= True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'misperriscontacto2018@gmail.com'
+EMAIL_HOST_PASSWORD = '123maipuperrisuc'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
